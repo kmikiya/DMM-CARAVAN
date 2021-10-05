b0b59f1b0f0e43fe45149817e4d17725f7f9a172
EC2 Default User <ec2-user@ip-172-31-90-41.ec2.internal>
2021-10-05 09:01:15 +0000

    therd

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,9 +9,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
+     @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+    render :new #同じコントローラ内の別アクションのViewを表示する。この場合newアクション
+    end
   end
 
   def show
@@ -19,6 +22,20 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)#blog_pathは引数で,そのURLにHTTPリクエストを実行。8章redirect toの流れ
+                               #この場合blogつまりshowアクションが実行され詳細画面に飛ぶ
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
